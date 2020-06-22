4dc620f27f9a7fbf6cbb2a525070bf4a40a9a899
jinleileiking <jinleileiking@gmail.com>
2020-06-22 16:23:26 +0800

    copy codes from leixiaohua and make it can build

--- publish.c
+++ publish.c
@@ -8,165 +8,165 @@
 #endif
  
  
-#include <libavutil/timestamp.h>
 #include <libavformat/avformat.h>
- 
-/**
- * @ingroup lavc_decoding
- * Required number of additionally allocated bytes at the end of the input bitstream for decoding.
- * This is mainly needed because some optimized bitstream readers read
- * 32 or 64 bit at once and could read over the end.
- * Note: If the first 23 bits of the additional bytes are not 0, then damaged
- * MPEG bitstreams could cause overread and segfault.
- */
-#define FF_INPUT_BUFFER_PADDING_SIZE 16
- 
-void copy_stream_info(AVStream* ostream, AVStream* istream, AVFormatContext* ofmt_ctx){
-    AVCodecContext* icodec = istream->codec;
-    AVCodecContext* ocodec = ostream->codec;
-    
-    ostream->id = istream->id;
-    ocodec->codec_id = icodec->codec_id;
-    ocodec->codec_type = icodec->codec_type;
-    ocodec->bit_rate = icodec->bit_rate;
-    
-    int extra_size = (uint64_t)icodec->extradata_size + FF_INPUT_BUFFER_PADDING_SIZE;
-    ocodec->extradata = (uint8_t*)av_mallocz(extra_size);
-    memcpy(ocodec->extradata, icodec->extradata, icodec->extradata_size);
-    ocodec->extradata_size= icodec->extradata_size;
-    
-    // Some formats want stream headers to be separate.
-    if (ofmt_ctx->oformat->flags & AVFMT_GLOBALHEADER){
-        ostream->codec->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
-    }
-}
- 
-void copy_video_stream_info(AVStream* ostream, AVStream* istream, AVFormatContext* ofmt_ctx){
-    copy_stream_info(ostream, istream, ofmt_ctx);
-    
-    AVCodecContext* icodec = istream->codec;
-    AVCodecContext* ocodec = ostream->codec;
-    
-    ocodec->width = icodec->width;
-    ocodec->height = icodec->height;
-    ocodec->time_base = icodec->time_base;
-    ocodec->gop_size = icodec->gop_size;
-    ocodec->pix_fmt = icodec->pix_fmt;
-}
- 
-void copy_audio_stream_info(AVStream* ostream, AVStream* istream, AVFormatContext* ofmt_ctx){
-    copy_stream_info(ostream, istream, ofmt_ctx);
-    
-    AVCodecContext* icodec = istream->codec;
-    AVCodecContext* ocodec = ostream->codec;
-    
-    ocodec->sample_fmt = icodec->sample_fmt;
-    ocodec->sample_rate = icodec->sample_rate;
-    ocodec->channels = icodec->channels;
-}
- 
-int main(int argc, char** argv){
+#include <libavutil/timestamp.h>
+#include <libavutil/mathematics.h>
+
+
+int main(int argc, char* argv[])
+{
     if (argc <= 2) {
         printf("Usage: %s  \n"
-            "%s a.flv rtmp://dev:1935/live/livestream\n", 
-            argv[0], argv[0]); 
+                "%s a.flv rtmp://dev:1935/live/livestream\n", 
+                argv[0], argv[0]); 
         exit(-1);
     }
-    
-    const char* filename = argv[1];
-    const char* url = argv[2];
-    
-    av_register_all();
-    avformat_network_init();
-    
-    // open format context
-    AVFormatContext* ifmt_ctx = NULL;
-    if(avformat_open_input(&ifmt_ctx, filename, NULL, NULL) != 0){
-        printf("error avformat_open_input\n");
-        return -1;
-    }
-    
-    if(avformat_find_stream_info(ifmt_ctx, NULL) < 0){
-        printf("error avformat_find_stream_info\n");
-        return -1;
-    }
-    
-    // create an output format context
-    AVFormatContext* ofmt_ctx = NULL;
-    if(avformat_alloc_output_context2(&ofmt_ctx, NULL, "flv", url) < 0){
-        printf("error avformat_alloc_output_context2\n");
-        return -1;
-    }
-    
-    // open file.
-    if(avio_open2(&ofmt_ctx->pb, url, AVIO_FLAG_WRITE, &ofmt_ctx->interrupt_callback, NULL) < 0){
-        printf("error avio_open2\n");
-        return -1;
-    }
-    
-    // create stream
-    int stream_index;
-    if((stream_index = av_find_best_stream(ifmt_ctx, AVMEDIA_TYPE_VIDEO, -1, -1, NULL, 0)) >= 0){
-        AVStream* istream = ifmt_ctx->streams[stream_index];
-        AVStream* ostream = avformat_new_stream(ofmt_ctx, NULL);
-        if(!ostream){
-            printf("error ostream video\n");
-            return -1;
-        }
-        
-        copy_video_stream_info(ostream, istream, ofmt_ctx);
-    }
-    if((stream_index = av_find_best_stream(ifmt_ctx, AVMEDIA_TYPE_AUDIO, -1, -1, NULL, 0)) >= 0){
-        AVStream* istream = ifmt_ctx->streams[stream_index];
-        AVStream* ostream = avformat_new_stream(ofmt_ctx, NULL);
-        if(!ostream){
-            printf("error ostream audio\n");
-            return -1;
-        }
-        
-        copy_audio_stream_info(ostream, istream, ofmt_ctx);
-    }
-    
-    av_dump_format(ofmt_ctx, 0, url, 1);
-    
-    // write header
-    if(avformat_write_header(ofmt_ctx, NULL) != 0){
-        printf("error avformat_write_header\n");
-        return -1;
-    }
-    
-    // init packet(compressed data)
+
+    const char* in_filename = argv[1];
+    const char* out_filename = argv[2];
+
+    AVOutputFormat *ofmt = NULL;
+    //Input AVFormatContext and Output AVFormatContext
+    AVFormatContext *ifmt_ctx = NULL, *ofmt_ctx = NULL;
     AVPacket pkt;
-    av_init_packet(&pkt);
-    
-    // read packet
-    int64_t last_pts = 0;
-    while(av_read_frame(ifmt_ctx, &pkt) >= 0){
-        if(av_interleaved_write_frame(ofmt_ctx, &pkt) < 0){
-            printf("error avformat_write_header\n");
-            return -1;
-        }
-        
-        double time_ms = 0;
-        AVStream* stream = ofmt_ctx->streams[pkt.stream_index];
-        if(stream->time_base.den > 0){
-            time_ms = (pkt.pts - last_pts) * stream->time_base.num * 1000 / stream->time_base.den;
-        }
-        
-        printf("write packet pts=%"PRId64", dts=%"PRId64", stream=%d sleep %.1fms\n", pkt.pts, pkt.dts, pkt.stream_index, time_ms);
-        
-        if(time_ms > 500){
-            av_usleep(time_ms * 1000);
-            last_pts = pkt.pts;
-        }
-        
-        av_free_packet(&pkt);
+    int ret, i;
+    int videoindex=-1;
+    int frame_index=0;
+    int64_t start_time=0;
+
+    av_register_all();
+    //Network
+    avformat_network_init();
+    //Input
+    if ((ret = avformat_open_input(&ifmt_ctx, in_filename, 0, 0)) < 0) {
+        printf( "Could not open input file.");
+        goto end;
     }
-    
-    // cleanup
-    avformat_free_context(ofmt_ctx);
+    if ((ret = avformat_find_stream_info(ifmt_ctx, 0)) < 0) {
+        printf( "Failed to retrieve input stream information");
+        goto end;
+    }
+
+    for(i=0; i<ifmt_ctx->nb_streams; i++) 
+        if(ifmt_ctx->streams[i]->codec->codec_type==AVMEDIA_TYPE_VIDEO){
+            videoindex=i;
+            break;
+        }
+
+    av_dump_format(ifmt_ctx, 0, in_filename, 0);
+
+    //Output
+
+    avformat_alloc_output_context2(&ofmt_ctx, NULL, "flv", out_filename); //RTMP
+    //avformat_alloc_output_context2(&ofmt_ctx, NULL, "mpegts", out_filename);//UDP
+
+    if (!ofmt_ctx) {
+        printf( "Could not create output context\n");
+        ret = AVERROR_UNKNOWN;
+        goto end;
+    }
+    ofmt = ofmt_ctx->oformat;
+    for (i = 0; i < ifmt_ctx->nb_streams; i++) {
+        //Create output AVStream according to input AVStream
+        AVStream *in_stream = ifmt_ctx->streams[i];
+        AVStream *out_stream = avformat_new_stream(ofmt_ctx, in_stream->codec->codec);
+        if (!out_stream) {
+            printf( "Failed allocating output stream\n");
+            ret = AVERROR_UNKNOWN;
+            goto end;
+        }
+        //Copy the settings of AVCodecContext
+        ret = avcodec_copy_context(out_stream->codec, in_stream->codec);
+        if (ret < 0) {
+            printf( "Failed to copy context from input to output stream codec context\n");
+            goto end;
+        }
+        out_stream->codec->codec_tag = 0;
+        if (ofmt_ctx->oformat->flags & AVFMT_GLOBALHEADER)
+            out_stream->codec->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+    }
+    //Dump Format------------------
+    av_dump_format(ofmt_ctx, 0, out_filename, 1);
+    //Open output URL
+    if (!(ofmt->flags & AVFMT_NOFILE)) {
+        ret = avio_open(&ofmt_ctx->pb, out_filename, AVIO_FLAG_WRITE);
+        if (ret < 0) {
+            printf( "Could not open output URL '%s'", out_filename);
+            goto end;
+        }
+    }
+    //Write file header
+    ret = avformat_write_header(ofmt_ctx, NULL);
+    if (ret < 0) {
+        printf( "Error occurred when opening output URL\n");
+        goto end;
+    }
+
+    start_time=av_gettime();
+    while (1) {
+        AVStream *in_stream, *out_stream;
+        //Get an AVPacket
+        ret = av_read_frame(ifmt_ctx, &pkt);
+        if (ret < 0)
+            break;
+        //FIX：No PTS (Example: Raw H.264)
+        //Simple Write PTS
+        if(pkt.pts==AV_NOPTS_VALUE){
+            //Write PTS
+            AVRational time_base1=ifmt_ctx->streams[videoindex]->time_base;
+            //Duration between 2 frames (us)
+            int64_t calc_duration=(double)AV_TIME_BASE/av_q2d(ifmt_ctx->streams[videoindex]->r_frame_rate);
+            //Parameters
+            pkt.pts=(double)(frame_index*calc_duration)/(double)(av_q2d(time_base1)*AV_TIME_BASE);
+            pkt.dts=pkt.pts;
+            pkt.duration=(double)calc_duration/(double)(av_q2d(time_base1)*AV_TIME_BASE);
+        }
+        //Important:Delay
+        if(pkt.stream_index==videoindex){
+            AVRational time_base=ifmt_ctx->streams[videoindex]->time_base;
+            AVRational time_base_q={1,AV_TIME_BASE};
+            int64_t pts_time = av_rescale_q(pkt.dts, time_base, time_base_q);
+            int64_t now_time = av_gettime() - start_time;
+            if (pts_time > now_time)
+                av_usleep(pts_time - now_time);
+
+        }
+
+        in_stream  = ifmt_ctx->streams[pkt.stream_index];
+        out_stream = ofmt_ctx->streams[pkt.stream_index];
+        /* copy packet */
+        //Convert PTS/DTS
+        pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base, out_stream->time_base, (AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
+        pkt.dts = av_rescale_q_rnd(pkt.dts, in_stream->time_base, out_stream->time_base, (AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
+        pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base, out_stream->time_base);
+        pkt.pos = -1;
+        //Print to Screen
+        if(pkt.stream_index==videoindex){
+            printf("Send %8d video frames to output URL\n",frame_index);
+            frame_index++;
+        }
+        //ret = av_write_frame(ofmt_ctx, &pkt);
+        ret = av_interleaved_write_frame(ofmt_ctx, &pkt);
+
+        if (ret < 0) {
+            printf( "Error muxing packet\n");
+            break;
+        }
+
+        av_free_packet(&pkt);
+
+    }
+    //Write file trailer
+    av_write_trailer(ofmt_ctx);
+end:
     avformat_close_input(&ifmt_ctx);
-    
-    printf("done\n");
+    /* close output */
+    if (ofmt_ctx && !(ofmt->flags & AVFMT_NOFILE))
+        avio_close(ofmt_ctx->pb);
+    avformat_free_context(ofmt_ctx);
+    if (ret < 0 && ret != AVERROR_EOF) {
+        printf( "Error occurred.\n");
+        return -1;
+    }
     return 0;
 }
